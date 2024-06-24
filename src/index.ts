08f75678b0c50a1f41324335f3d3ec349e45df37
Revadike <4411977+Revadike@users.noreply.github.com>
2024-06-24 10:52:25 +0200

    Fix import

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import os from 'node:os'
 import fs from 'node:fs'
 import path from 'node:path'
-import sleep from './utils/sleep'
+import sleep from './utils/sleep.mjs'
 
 const BASE_URL = 'https://start.exactonline.nl/api/v1/'
 
